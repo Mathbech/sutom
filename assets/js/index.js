f54c8b32427e8ac70db72e516845c013909051e4
Mathbech <legeek.you@gmail.com>
2023-05-02 11:34:42 +0200

    3

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,22 @@
 window.addEventListener('load', create_table);
 
+
 function create_table() {
     var table = document.getElementById("grille");
 
     var tb1 = document.createElement("table");
     var tb1boby = document.createElement("tbody");
 
-    for (var i = 0; i < 2; i++) {
+    for (var i = 0; i < 6; i++) {
         // creates a table row
         var row = document.createElement("tr");
 
-        for (var j = 0; j < 2; j++) {
+        for (var j = 0; j < 6; j++) {
             // Create a <td> element and a text node, make the text
             // node the contents of the <td>, and put the <td> at
             // the end of the table row
             var cell = document.createElement("td");
-            var cellText = document.createTextNode("cellule " + i + ", colonne " + j);
+            var cellText = document.createTextNode(".");
             cell.appendChild(cellText);
             row.appendChild(cell);
         }
