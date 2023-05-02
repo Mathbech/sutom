bf4716b991e94ee388c79fbcc1a19a015d3360aa
Mathbech <legeek.you@gmail.com>
2023-05-02 10:38:41 +0200

    Début de création du tableau

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+function create_table() {
+    var table = document.getElementById("grille");
+
+    var tb1 = document.createElement("table");
+    var tb1boby = document.createElement("tbody");
+
+    for (var i = 0; i < 2; i++) {
+        // creates a table row
+        var row = document.createElement("tr");
+
+        for (var j = 0; j < 2; j++) {
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+            var cellText = document.createTextNode("cell in row " + i + ", column " + j);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        // add the row to the end of the table body
+        tb1boby.appendChild(row);
+    }
+
+    // put the <tbody> in the <table>
+    tb1.appendChild(tb1boby);
+    // appends <table> into <body>
+    table.appendChild(tb1);
+    // sets the border attribute of tbl to 2;
+    tb1.setAttribute("border", "2");
+}
