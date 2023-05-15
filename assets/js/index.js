4a0bcfc15d00b009feaca2e30ba9fc512db6b132
Bébech <legeek.you@gmail.com>
2023-05-15 14:34:29 +0200

    Changement de fichier pour l'affichage du clavier

--- assets/js/index.js
+++ assets/js/index.js
@@ -40,45 +40,4 @@ function create_table() {
   tb1.appendChild(tb1boby);
   table.appendChild(tb1);
   tb1.setAttribute("border", "2");
-
-  // Créer un clavier
-  var keyboard = document.createElement("div");
-  keyboard.setAttribute("id", "clavier");
-
-  var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var rows = 3;
-  var lettersPerRow = Math.ceil(letters.length / rows);
-  var index = 0;
-
-  for (var i = 0; i < rows; i++) {
-    var row = document.createElement("div");
-    for (var j = 0; j < lettersPerRow; j++) {
-      if (index < letters.length) {
-        var button = document.createElement("button");
-        button.innerHTML = letters[index];
-        button.onclick = function() {
-          var lettre = this.innerHTML;
-          var cells = document.getElementsByTagName("td");
-          for (var k = 0; k < cells.length; k++) {
-            if (cells[k].innerHTML == "." && cells[k].parentNode.rowIndex == i+1) {
-              cells[k].innerHTML = lettre;
-              break;
-            }
-          }
-        };
-        row.appendChild(button);
-        index++;
-      }
-    }
-    keyboard.appendChild(row);
-  }
-
-  // Afficher le clavier sur plusieurs lignes
-  var keyboardRows = keyboard.getElementsByTagName("div");
-  for (var i = 0; i < keyboardRows.length; i++) {
-    keyboardRows[i].style.display = "inline-block";
-    keyboardRows[i].style.marginBottom = "10px";
-  }
-
-  table.after(keyboard);
 }
--- assets/js/saisie.js
+++ assets/js/saisie.js
@@ -1,10 +1,45 @@
-window.addEventListener("keydown", ajouterLettre());
+window.addEventListener('load', ajouterLettre);
 
 function ajouterLettre() {
-    let tableau = document.getElementById("grille");
-    let lettreInput = document.getElementById("lettreInput");
-    let lettre = lettreInput.value;
-    let newRow = tableau.insertRow(-1);
-    let cell = newRow.insertCell(0);
-    cell.innerHTML = lettre;
+    // Créer un clavier
+    var table = document.getElementById('grille')
+    var keyboard = document.createElement("div");
+    keyboard.setAttribute("id", "clavier");
+
+    var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var rows = 1;
+    var lettersPerRow = Math.ceil(letters.length / rows);
+    var index = 0;
+
+    for (var i = 0; i < rows; i++) {
+        var row = document.createElement("div");
+        for (var j = 0; j < lettersPerRow; j++) {
+            if (index < letters.length) {
+                var button = document.createElement("button");
+                button.innerHTML = letters[index];
+                button.onclick = function () {
+                    var lettre = this.innerHTML;
+                    var cells = document.getElementsByTagName("td");
+                    for (var k = 0; k < cells.length; k++) {
+                        if (cells[k].innerHTML == "." && cells[k].parentNode.rowIndex == i + 1) {
+                            cells[k].innerHTML = lettre;
+                            break;
+                        }
+                    }
+                };
+                row.appendChild(button);
+                index++;
+            }
+        }
+        keyboard.appendChild(row);
+    }
+
+    // Afficher le clavier sur plusieurs lignes
+    var keyboardRows = keyboard.getElementsByTagName("div");
+    for (var i = 0; i < keyboardRows.length; i++) {
+        keyboardRows[i].style.display = "inline-block";
+        keyboardRows[i].style.marginBottom = "10px";
+    }
+
+    table.after(keyboard);
 }
