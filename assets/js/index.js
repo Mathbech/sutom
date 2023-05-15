07694793da5badb805d2d1fe1e795a19e70c7543
Bébech <legeek.you@gmail.com>
2023-05-15 16:12:28 +0200

    Modification de la saisie

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,43 +1,48 @@
 window.addEventListener('load', create_table);
 
 function create_table() {
-  // Define table of words
-  const mots = ['CHAT', 'CHIEN', 'OISEAU', 'POISSON', 'LAPIN', 'TARTUFF', 'TARTIFLETTE'];
+    // Define table of words
+    //   const mots = ['CHAT', 'CHIEN', 'OISEAU', 'POISSON', 'LAPIN', 'TARTUFF', 'TARTIFLETTE'];
 
-  // Générer un index aléatoire pour sélectionner un mot du tableau
-  const indexAleatoire = Math.floor(Math.random() * mots.length);
+    fetch('./assets/dico/dico.json')
+        .then(response => response.json())
+        .then(data => {
+            const words = data.words;
+            const mots = words[Math.floor(Math.random() * words.length)];
 
-  // Récupérer le mot correspondant à l'index aléatoire
-  const motChoisi = mots[indexAleatoire];
+            // Print word in console.log
+            console.log(`Le mot choisi est : ${mots}`);
 
-  // Print word in console.log
-  console.log(`Le mot choisi est : ${motChoisi}`);
+            // Créer une grille de mots
+            var table = document.getElementById("grille");
+            var tb1 = document.createElement("table");
+            var tb1boby = document.createElement("tbody");
 
-  // Créer une grille de mots
-  var table = document.getElementById("grille");
-  var tb1 = document.createElement("table");
-  var tb1boby = document.createElement("tbody");
+            for (var i = 0; i < 6; i++) {
+                // creates a table row
+                var row = document.createElement("tr");
 
-  for (var i = 0; i < 6; i++) {
-    // creates a table row
-    var row = document.createElement("tr");
+                for (var j = 0; j < mots.length; j++) {
+                    var cell = document.createElement("td");
+                    var letter;
+                    if (j == 0) {
+                        letter = document.createTextNode(mots[0]);
+                    } else {
+                        letter = document.createTextNode(".");
+                    }
+                    cell.appendChild(letter);
+                    row.appendChild(cell);
+                }
 
-    for (var j = 0; j < motChoisi.length; j++) {
-      var cell = document.createElement("td");
-      var letter;
-      if (j == 0) {
-        letter = document.createTextNode(motChoisi[0]);
-      } else {
-        letter = document.createTextNode(".");
-      }
-      cell.appendChild(letter);
-      row.appendChild(cell);
-    }
+                tb1boby.appendChild(row);
+            }
 
-    tb1boby.appendChild(row);
-  }
+            tb1.appendChild(tb1boby);
+            table.appendChild(tb1);
+            tb1.setAttribute("border", "2");
 
-  tb1.appendChild(tb1boby);
-  table.appendChild(tb1);
-  tb1.setAttribute("border", "2");
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite :', error);
+        });
 }
--- assets/js/saisie.js
+++ assets/js/saisie.js
@@ -1,42 +1,47 @@
-window.addEventListener('load', ajouterClavier);
+window.addEventListener('load', ajouterLettre);
 
-function ajouterClavier() {
-  var table = document.getElementById('grille');
-  var keyboard = document.createElement('div');
-  keyboard.setAttribute('id', 'clavier');
+function ajouterLettre() {
+    // Créer un clavier
+    var table = document.getElementById('grille')
+    var keyboard = document.createElement("div");
+    keyboard.setAttribute("id", "clavier");
 
-  var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var rows = 1;
-  var lettersPerRow = Math.ceil(letters.length / rows);
-  var index = 0;
+    var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var rows = 1;
+    var lettersPerRow = Math.ceil(letters.length / rows);
+    var index = 0;
+    for (var i = 0; i < rows; i++) {
+        var row = document.createElement("div");
+        for (var j = 0; j < lettersPerRow; j++) {
+            if (index < letters.length) {
+                var button = document.createElement("button");
+                button.innerHTML = letters[index];
+                button.onclick = function () {
+                    var lettre = this.innerHTML;
+                    var cells = document.getElementsByTagName("td");
+                    for (var k = 0; k < cells.length; k++) {
+                        if (cells[k].innerHTML == "." && cells[k].parentNode.rowIndex == i-1) {
 
-  for (var i = 0; i < rows; i++) {
-    var row = document.createElement('div');
-    row.classList.add('keyboard-row');
-
-    for (var j = 0; j < lettersPerRow; j++) {
-      if (index < letters.length) {
-        var button = document.createElement('button');
-        button.classList.add('keyboard-button');
-        button.innerHTML = letters[index];
-        button.onclick = function () {
-          var lettre = this.innerHTML;
-          var cells = document.getElementsByTagName('td');
-          for (var k = 0; k < cells.length; k++) {
-            if (cells[k].innerHTML == '.' && cells[k].parentNode.rowIndex == i + 1) {
-              cells[k].innerHTML = lettre;
-              break;
+                        // if (cells[k].innerHTML == "." && cells[k].parentNode.rowIndex == i + 1) {
+                            cells[k].innerHTML = lettre;
+                            break;
+                        }
+                        
+                    }
+                };
+                row.appendChild(button);
+                index++;
             }
-          }
-        };
-
-        row.appendChild(button);
-        index++;
-      }
+        }
+        keyboard.appendChild(row);
     }
 
-    keyboard.appendChild(row);
-  }
+    // Afficher le clavier sur plusieurs lignes
+    var keyboardRows = keyboard.getElementsByTagName("div");
+    for (var i = 0; i < keyboardRows.length; i++) {
+        keyboardRows[i].style.display = "inline-block";
+        keyboardRows[i].style.marginBottom = "10px";
+    }
 
-  table.after(keyboard);
+    table.after(keyboard);
 }
